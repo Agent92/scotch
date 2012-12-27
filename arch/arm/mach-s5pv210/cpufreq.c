6ee13fb5f3d1b95261d39c2f0bb29d6074bb78c8
Stefan Duffy <agent92@agent92-ubuntu.(none)>
2012-12-27 16:00:15 +0000

    1320MHz step, 1200MHz step + 10% fsb OC.

--- arch/arm/mach-s5pv210/cpufreq.c
+++ arch/arm/mach-s5pv210/cpufreq.c
@@ -33,8 +33,8 @@ static struct cpufreq_freqs freqs;
 static DEFINE_MUTEX(set_freq_lock);
 
 
-#define APLL_VAL_1375    ((1<<31)|(165<<16)|(3<<8)|(1))
-#define APLL_VAL_1250    ((1<<31)|(150<<16)|(3<<8)|(1))
+#define APLL_VAL_1320    ((1<<31)|(165<<16)|(3<<8)|(1))
+#define APLL_VAL_1200    ((1<<31)|(150<<16)|(3<<8)|(1))
 #define APLL_VAL_1000	((1<<31)|(125<<16)|(3<<8)|(1))
 #define APLL_VAL_800	((1<<31)|(100<<16)|(3<<8)|(1))
 
@@ -80,21 +80,21 @@ enum s5pv210_dmc_port {
 
 static struct cpufreq_frequency_table s5pv210_freq_table[] = {
 
-	{L0, 1375*1000},
-	{L1, 1250*1000},
-	{L2, 1000*1000},
-	{L3, 800*1000},
-   	{L4, 400*1000},
-	{L5, 200*1000},
-	{L6, 100*1000},
-	{0, CPUFREQ_TABLE_END},};
+		{L0, 1320*1000},
+		{L1, 1200*1000},
+		{L2, 1000*1000},
+		{L3, 800*1000},
+   	        {L4, 400*1000},
+		{L5, 200*1000},
+		{L6, 100*1000},
+{0, CPUFREQ_TABLE_END},};
 
 static struct regulator *arm_regulator;
 static struct regulator *internal_regulator;
 
 struct s5pv210_dvs_conf {
-	unsigned long	arm_volt; /* uV */
-	unsigned long	int_volt; /* uV */
+unsigned long	arm_volt; /* uV */
+unsigned long	int_volt; /* uV */
 };
 
 #ifdef CONFIG_CUSTOM_VOLTAGE
@@ -106,57 +106,55 @@ const unsigned long int_volt_max = 1300000;
 #endif
 
 static struct s5pv210_dvs_conf dvs_conf[] = {
-	[L0] = {
-
-		.arm_volt   = 1400000,
-		.int_volt   = 1200000,
-		},
-	[L1] = {
-		.arm_volt   = 1325000,
-		.int_volt   = 1200000,
-		},
-	[L2] = {
-		.arm_volt   = 1250000,
-		.int_volt   = 1100000,
-		},
-	[L3] = {
-		.arm_volt   = 1200000,
-		.int_volt   = 1100000,
-		},
-    	[L4] = {
-		.arm_volt   = 1050000,
-		.int_volt   = 1100000,
-		},
-	[L5] = {
-		.arm_volt   = 950000,
-		.int_volt   = 1100000,
-		},
-	[L6] = {
-		.arm_volt   = 950000,
-		.int_volt   = 1000000,
-		}
+[L0] = {
+	.arm_volt = 1400000,
+	.int_volt = 1150000,
+},
+[L1] = {
+	.arm_volt = 1300000,
+	.int_volt = 1150000,
+},
+[L2] = {
+	.arm_volt = 1250000,
+	.int_volt = 1100000,
+},
+[L3] = {
+	.arm_volt = 1200000,
+	.int_volt = 1100000,
+},
+     [L4] = {
+	.arm_volt = 1050000,
+	.int_volt = 1100000,
+},
+[L5] = {
+	.arm_volt = 950000,
+	.int_volt = 1100000,
+},
+[L6] = {
+	.arm_volt = 950000,
+	.int_volt = 1000000,
+}
 };
 
 static u32 clkdiv_val[7][11] = {
-	/*{ APLL, A2M, HCLK_MSYS, PCLK_MSYS,
-	 * HCLK_DSYS, PCLK_DSYS, HCLK_PSYS, PCLK_PSYS, ONEDRAM,
-	 * MFC, G3D }
-	 */
-
-	/* L0 : [1375/200/200/100][166/83][133/66][200/200] */
-	{0, 6, 6, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L1 : [1250/200/200/100][166/83][133/66][200/200] */
-	{0, 5, 5, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L2 : [1000/200/200/100][166/83][133/66][200/200] */
-	{0, 4, 4, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L3 : [800/200/200/100][166/83][133/66][200/200] */
-	{0, 3, 3, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L4 : [400/200/200/100][166/83][133/66][200/200] */
-	{1, 3, 1, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L5 : [200/200/200/100][166/83][133/66][200/200] */
-	{3, 3, 0, 1, 3, 1, 4, 1, 3, 0, 0},
-	/* L6 : [100/100/100/100][83/83][66/66][100/100] */
-	{7, 7, 0, 0, 7, 0, 9, 0, 7, 0, 0}
+/*{ APLL, A2M, HCLK_MSYS, PCLK_MSYS,
+* HCLK_DSYS, PCLK_DSYS, HCLK_PSYS, PCLK_PSYS, ONEDRAM,
+* MFC, G3D }
+*/
+/* L0 : [1320/200/200/100][166/83][133/66][200/200] */
+{0, 6, 6, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L1 : [1200/200/200/100][166/83][133/66][200/200] i.e. morfic's T11 config*/
+{0, 5, 5, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L2 : [1000/200/200/100][166/83][133/66][200/200] */
+{0, 4, 4, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L3 : [800/200/200/100][166/83][133/66][200/200] */
+{0, 3, 3, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L4 : [400/200/200/100][166/83][133/66][200/200] */
+{1, 3, 1, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L5 : [200/200/200/100][166/83][133/66][200/200] */
+{3, 3, 0, 1, 3, 1, 4, 1, 3, 0, 0},
+/* L6 : [100/100/100/100][83/83][66/66][100/100] */
+{7, 7, 0, 0, 7, 0, 9, 0, 7, 0, 0}
 };
 
 /*
@@ -253,7 +251,7 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 	if (index == L2)
 	{
 
-		if (policy->user_policy.max >= 1250000)
+		if (policy->user_policy.max >= 1200000)
 			index = L1;
 
 		freqs.new = s5pv210_freq_table[index].frequency;		
@@ -263,8 +261,8 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 	int_volt = dvs_conf[index].int_volt;
 
 
-	/* Increase voltages for FSB if 1.25GHz is selected */
-	if ((policy->user_policy.max >= 1250000) && (index != L0) && (index != L1) && (index !=L6))
+	/* Increase voltages for FSB if 1.2GHz is selected */
+	if ((policy->user_policy.max >= 1200000) && (index != L0) && (index != L1) && (index !=L6))
 
 	    int_volt += 50000;
 
@@ -410,14 +408,14 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 
 		case L0:
 
-		/* APLL FOUT becomes 1375 Mhz */
-		__raw_writel(APLL_VAL_1375, S5P_APLL_CON);
+		/* APLL FOUT becomes 1320 Mhz */
+		__raw_writel(APLL_VAL_1320, S5P_APLL_CON);
 
 		break;            
 
 		case L1:
-		/* APLL FOUT becomes 1250 Mhz */
-		__raw_writel(APLL_VAL_1250, S5P_APLL_CON);
+		/* APLL FOUT becomes 1200 Mhz */
+		__raw_writel(APLL_VAL_1200, S5P_APLL_CON);
 		break;            
 		
 		case L2:
@@ -482,7 +480,7 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 		if (!bus_speed_changing)
 		{
 
-			if (policy->user_policy.max >= 1250000)
+			if (policy->user_policy.max >= 1200000)
 
 			{
 				s5pv210_set_refresh(DMC1, 220000);
@@ -509,11 +507,11 @@ static int s5pv210_target(struct cpufreq_policy *policy,
 		{
 			/*
 			 * DMC0 : 166Mhz
-			 * DMC1 : 200MHz or 220Mhz if OC'ing to 1250MHz CPU (L1)
+			 * DMC1 : 200MHz or 220Mhz if OC'ing to 1200MHz CPU (L1)
 			 */
 			s5pv210_set_refresh(DMC0, 166000);
 
-			if (policy->user_policy.max >= 1250000)
+			if (policy->user_policy.max >= 1200000)
 
 			{
 				s5pv210_set_refresh(DMC1, 220000); //raised 220MHz FSB
@@ -596,7 +594,7 @@ void customvoltage_updateintvolt(unsigned long * int_voltages)
 	int i;
 
 	mutex_lock(&set_freq_lock);
-	for (i = 0; i < 2; i++) {  // only allow int volts for 1.2GHz and 1.45GHz
+	for (i = 0; i < 2; i++) {  // only allow int volts for 1.2GHz and 1.32GHz
 		if (int_voltages[i] > int_volt_max)
 		    int_voltages[i] = int_volt_max;
 		dvs_conf[i].int_volt = int_voltages[i];
